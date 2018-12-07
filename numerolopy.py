580f4bb0ac6a2212b54d40bda27cd8a329fffec3
Flávio Tassan <tassanf@gmail.com>
2018-12-06 23:04:31 -0200

    transforming the letters to numbers, and sum them

--- numerolopy.py
+++ numerolopy.py
@@ -4,6 +4,29 @@ import name as nm
 
 def letters_to_numbers(your_name):
     name_array = nm.name_to_array(your_name)
-
+    nmbs = []
     for c in name_array:
+        nmbs.append(ltr.letter_to_number(c))
+
+    return nmbs
+
+
+def number_len(number):
+    return len(str(abs(number)))
+
+
+def sum_list(numbers_list):
+    result = sum(numbers_list)
+
+    if number_len(result) > 2:
         pass
+
+# sum digits
+
+
+name = 'Flavio'
+numbers = letters_to_numbers(name)
+
+print(letters_to_numbers(name))
+
+print(sum_list(numbers))
