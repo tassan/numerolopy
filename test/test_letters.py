2251f3d17bc78c4f601d0ee69c52712e4a5fd443
Flávio Tassan <tassanf@gmail.com>
2018-12-06 22:36:22 -0200

    tests

---
+++ test/test_letters.py
@@ -0,0 +1,7 @@
+import letters as ltr
+
+letter = 'a'
+number = 1
+
+
+assert ltr.letter_to_number(letter) == number
---
+++ test/test_name.py
@@ -0,0 +1,5 @@
+import name
+
+you = 'FlaviO'
+
+assert name.name_to_array(you) == ['f', 'l', 'a', 'v', 'i', 'o']
