ea3be4b2f47dc58bc393ae3e00d4cdcd5b7fc925
Flavio Tassan dos Santos Filho <flavio.tassan>
2018-12-13 07:59:12 -0200

    name tests

--- test/test_name.py
+++ test/test_name.py
@@ -1,5 +1,13 @@
-import name
+from names import name
 
-you = 'FlaviO'
+person_name = 'Flavio Tassan'
+name_array = ['f', 'l', 'a', 'v', 'i', 'o']
+fullname_array = ['Flavio', 'Tassan']
+name_Uppercase = 'FLAVIO'
 
-assert name.name_to_array(you) == ['f', 'l', 'a', 'v', 'i', 'o']
+
+assert name.name_to_array(person_name) == fullname_array
+
+assert name.to_lowercase(name_Uppercase) == 'flavio'
+
+assert name.letters_to_array('flavio') == name_array
